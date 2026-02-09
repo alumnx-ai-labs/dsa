9afad536a00bb030c3bf8b8afa462c4d8b6fa3d1
KETHE PRASHANTH <133996543+ketheprashanth@users.noreply.github.com>
2026-02-09 16:31:22 +0530

    sending some changes

--- routers/twosum.py
+++ routers/twosum.py
@@ -1,16 +1,16 @@
-from fastapi import FastAPI
+from fastapi import APIRouter
 
-app = FastAPI()
+router = APIRouter()
 
-@app.get("/")
+@router.get("/two-sum")
 def solve_twosum(nums: str, target: int):
     arr = list(map(int, nums.split(",")))
     lookup = {}
-
+    
     for i, n in enumerate(arr):
         diff = target - n
         if diff in lookup:
             return {"indices": [lookup[diff], i]}
         lookup[n] = i
-
+    
     return {"message": "No solution"}
